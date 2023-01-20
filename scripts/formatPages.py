262d6169d8220542fa1d54ee0cb419def20eec56
mabarber92 <mathew.barber@aku.edu>
2023-01-20 10:15:50 +0000

    fixes to metadata in script, reorganisation of index pages

--- scripts/formatPages.py
+++ scripts/formatPages.py
@@ -43,18 +43,54 @@ def formatPages(pageToTemplate = "https://openiti.org/about.html", baseUrl = "ht
     assetPath = 'src= "{}/assets'.format(baseUrl)
     pageHtml = re.sub(r'src\s?=\s?"/assets', assetPath, pageHtml)
     
-    # Split text on <main> and </main> to get page template to wrap around new elements
-    preMainHtml = re.split(r"<main", pageHtml)[0]
+    # Split text on <main> and </main> or <div class="inner">[^>]+<div class="intro-text"> to get page template to wrap around new elements
+    if len(re.findall('<div class="inner">[^>]+<div class="intro-text">', pageHtml)) > 0:
+        preMainHtml = re.split('<div class="inner">[^>]+<div class="intro-text">', pageHtml)[0]
+    else:
+        preMainHtml = re.split(r"<main", pageHtml)[0]
     postMainHtml = re.split(r"</main>", pageHtml)[-1]
     
+    # Clean up jekyll meta that's not useful
+    metaCleanRegex = r'<meta property="og:url[^>]+>|<link rel="canonical"[^>]+>|<script type="application/ld\+json">[^>]+>'
+    preMainHtml = re.sub(metaCleanRegex, "", preMainHtml)
     
+    # Fetch parts of metadata to customise on pages
+    htmlTitle = re.findall(r'<title>[^>]+>', preMainHtml)
+    if len(htmlTitle) > 0:
+        htmlTitle = htmlTitle[0]
+    else:
+        htmlTitle = None
+    metaTitle = re.findall(r'<meta property="og:title"[^>]+>', preMainHtml)
+    if len(metaTitle) > 0:
+        metaTitle = metaTitle[0]
+    else:
+        metaTitle = None
+    twitterTitle = re.findall(r'<meta property="twitter:title"[^>]+>', preMainHtml)
+    if len(twitterTitle) > 0:        
+        twitterTitle = twitterTitle[0]
+    else:
+        twitterTitle = None
+    
+    print([htmlTitle, metaTitle, twitterTitle])
     # Loop through to create an index of author URIs linking to index pages for authors
     # For each author page create index of document pages
     # For every page, wrap in the existing html, and for document pages ensure the doc-specific stylesheet is used
     authorIndexTitle = '''<header class="section-title">
                           <h2>Digital Publications</h2>
                           </header>'''
-    authorIndex = [preMainHtml, '<main class="page-content" aria-label="Content">', '<div class="index inner">', authorIndexTitle]
+    
+    # Edit page metadata to reflex page
+    preMainHtmlAuthorIndex = preMainHtml[:]
+    if htmlTitle:
+        newTitle = '<title>Digital Publications | Open Islamicate Texts Initiative</title>'        
+        preMainHtmlAuthorIndex = re.sub(htmlTitle, newTitle, preMainHtmlAuthorIndex)
+    if metaTitle:
+        newTitle = '<meta property="og:title" content="Digital Publications" />'      
+        preMainHtmlAuthorIndex = re.sub(metaTitle, newTitle, preMainHtmlAuthorIndex)
+    if twitterTitle:
+        newTitle = '<meta property="twitter:title" content="Digital Publications" />'        
+        preMainHtmlAuthorIndex = re.sub(twitterTitle, newTitle, preMainHtmlAuthorIndex)
+    authorIndex = [preMainHtmlAuthorIndex, '<main class="page-content" aria-label="Content">', '<div class="index inner">', authorIndexTitle]
     for author in dirDict.keys():
         documentIndexTitle = '''<header class="section-title">
                           <h2>
@@ -65,8 +101,20 @@ def formatPages(pageToTemplate = "https://openiti.org/about.html", baseUrl = "ht
                         </svg></span>
                           {}</h2>
                           </header>'''.format(author)
-        documentIndex = [preMainHtml, '<main class="page-content" aria-label="Content">', '<div class="index inner">', documentIndexTitle]
-        authorPage = './authors/{}.html'.format(author)
+        # Edit page metadata to reflex page
+        preMainHtmlDocumentIndex = preMainHtml[:]
+        if htmlTitle:
+            newTitle = '<title>{} | Open Islamicate Texts Initiative</title>'.format(author)        
+            preMainHtmlDocumentIndex = re.sub(htmlTitle, newTitle, preMainHtmlDocumentIndex)
+        if metaTitle:
+            newTitle = '<meta property="og:title" content="{}" />'.format(author)      
+            preMainHtmlDocumentIndex = re.sub(metaTitle, newTitle, preMainHtmlDocumentIndex)
+        if twitterTitle:
+            newTitle = '<meta property="twitter:title" content="{}" />'.format(author)        
+            preMainHtmlDocumentIndex = re.sub(twitterTitle, newTitle, preMainHtmlDocumentIndex)
+        
+        documentIndex = [preMainHtmlDocumentIndex, '<main class="page-content" aria-label="Content">', '<div class="index inner">', documentIndexTitle]
+        authorPage = './authors/{}/index.html'.format(author)
         authorLinkHtml = '''<h3 class="entry-title">
             <a href="{}" class="more-link">{}<span class="icon icon--arrow-right">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="50.4 -114.8 16 16">
@@ -74,8 +122,9 @@ def formatPages(pageToTemplate = "https://openiti.org/about.html", baseUrl = "ht
             </svg></span></a></h3>'''.format(authorPage,author)
         authorIndex.append(authorLinkHtml)
         for document in dirDict[author]:
-            documentPage = "./{}/{}".format(author, document)
-            print(documentPage)
+            documentName = ".".join(document.split(".")[1:3])
+            documentPage = "./{}".format(document)
+            
             documentLinkHtml = '''<h3 class="entry-title">
             <a href="{}" class="more-link">{}<span class="icon icon--arrow-right">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="50.4 -114.8 16 16">
@@ -91,7 +140,7 @@ def formatPages(pageToTemplate = "https://openiti.org/about.html", baseUrl = "ht
                         <path d="M63.1-107.7l-6.7-6.7c-.2-.3-.6-.4-.9-.4-.4 0-.7.1-.9.4l-.8.8c-.3.3-.4.6-.4.9 0 .4.1.7.4.9l5 5-5 5c-.3.3-.4.6-.4.9 0 .4.1.7.4.9l.8.8c.3.3.6.4.9.4.4 0 .7-.1.9-.4l6.7-6.7c.3-.3.4-.6.4-.9 0-.4-.2-.7-.4-.9z"/>
                         </svg></span>
                           {}</h2>
-                          </header>'''.format(author, ".".join(document.split(".")[1:3]))
+                          </header>'''.format(author, documentName)
             
             documentPath = os.path.join(dataDir, "{}/{}".format(author, document))
             with open(documentPath, "r", encoding='utf-8-sig') as f:                
@@ -102,16 +151,29 @@ def formatPages(pageToTemplate = "https://openiti.org/about.html", baseUrl = "ht
             
             
             textContent = "\n".join(re.split(r"(</?body>)", textContent)[1:4])
-            print(textContent)
+            
             
             preMainHtmlSplit = re.split(r'(<link rel="stylesheet"[^>]+>)', preMainHtml)
             
             print(preMainHtmlSplit[1])
             for sheet in stylesheetIn:
                 if sheet not in stylesheetTemp:
-                    print(sheet)
+                    
                     preMainHtmlSplit.insert(2, sheet)
             preMainHtmlForDoc = "\n".join(preMainHtmlSplit)
+            
+            # Edit page metadata to reflex page
+           
+            if htmlTitle:
+                newTitle = '<title>{}| Open Islamicate Texts Initiative</title>'.format(documentName)        
+                preMainHtmlForDoc = re.sub(htmlTitle, newTitle, preMainHtmlForDoc)
+            if metaTitle:
+                newTitle = '<meta property="og:title" content="{}" />'.format(documentName)      
+                preMainHtmlForDoc = re.sub(metaTitle, newTitle, preMainHtmlForDoc)
+            if twitterTitle:
+                newTitle = '<meta property="twitter:title" content="{}" />'.format(documentName)        
+                preMainHtmlForDoc = re.sub(twitterTitle, newTitle, preMainHtmlForDoc)
+            
             fullHtml = "\n".join([preMainHtmlForDoc, '<main class="page-content" aria-label="Content">', '<div class="index inner">', documentTitle, textContent, postMainHtml])
             
             with open(documentPath, "w", encoding = "utf-8-sig") as f:
@@ -128,8 +190,8 @@ def formatPages(pageToTemplate = "https://openiti.org/about.html", baseUrl = "ht
         f.write(authorIndexHtml)
         f.close()
 
-if __name__ == '__main__':
-    formatPages()
+# if __name__ == '__main__':
+#     formatPages()
             
         
         
